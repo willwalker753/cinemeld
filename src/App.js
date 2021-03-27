8cd535e70ca9fd8d2a23878d636249f93eb8ff38
Will Walker <williamwalker753@gmail.com>
2021-03-26 23:45:08 -0500

    sidebar nav and home infinite scroll

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Nav from './components/nav/nav';
 import axios from 'axios';
 import apiURL from './components/util/apiURL';
+import InfiniteScroll from "react-infinite-scroll-component";
 import './app.css';
 
 export default class App extends Component {
@@ -9,30 +10,58 @@ export default class App extends Component {
     super(props)
     this.state = {
       movieList: [],
+      page: 3,
     }
+    this.getMoreData = this.getMoreData.bind(this);
   }
   componentDidMount() {
-    axios.get(apiURL() + '/')
+    axios.get(apiURL() + '/home?page=1')
       .then(res => {
-        console.log(res);
         this.setState({ movieList: res.data })
+    });
+    axios.get(apiURL() + '/home?page=2')
+      .then(res => {
+        let tempMovieList = this.state.movieList;
+        tempMovieList = tempMovieList.concat(res.data);
+        this.setState({ movieList: tempMovieList })
+    });
+  }
+  getMoreData() {
+    if(this.state.page < 50) {
+      axios.get(apiURL() + '/home?page=' + this.state.page)
+      .then(res => {
+        let tempMovieList = this.state.movieList;
+        tempMovieList = tempMovieList.concat(res.data);
+        let nextPage = this.state.page + 1;
+        this.setState({ 
+          movieList: tempMovieList,
+          page: nextPage
+        });
       })
+    }
   }
   render() {
     let { movieList } = this.state;
     return (
       <>
         <Nav />
-        <div className='app-movie-box'>
-          {movieList.map((movie, index) => (
-            <img 
-              key={index} 
-              className ='app-movie' 
-              src={'https://image.tmdb.org/t/p/w500/'+movie.poster_path} 
-              alt='movie poster'>
-            </img>
-          ))}
-        </div>
+        <InfiniteScroll
+          dataLength={this.state.movieList.length}
+          next={this.getMoreData}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          <div className='app-movie-box'>
+            {movieList.map((movie, index) => (
+              <img 
+                key={index} 
+                className ='app-movie' 
+                src={'https://image.tmdb.org/t/p/w500/'+movie.poster_path} 
+                alt={movie.title}>
+              </img>
+            ))}
+          </div>
+        </InfiniteScroll>
       </>
     )
   }
--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -7,14 +7,27 @@ export default class Nav extends Component {
     this.state = {
 
     }
+    this.account = this.account.bind(this);
   }
   componentDidMount() {
     
   }
+  account = () => {
+      console.log('something')
+  }
   render() {
     return (
       <div id='nav-box'>
-        <img src=''></img>
+        <a href='/'>
+            <img 
+                id='nav-logo' 
+                title='Home'
+                src='https://github.com/willwalker753/cinemeld/blob/main/public/android-chrome-192x192.png?raw=true' 
+                alt='logo'>
+            </img>
+        </a>
+        <i className="fas fa-user-circle" title='Account' onClick={ this.account }></i>
+        <i className="fas fa-search" title='Search'></i>
       </div>
     )
   }
