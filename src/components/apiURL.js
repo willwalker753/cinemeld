2dd248b042cd335cc4b4248afc6e8715ec6fa328
Will Walker <williamwalker753@gmail.com>
2021-03-28 00:19:51 -0500

    fixed dev env detection

--- src/components/apiURL.js
+++ src/components/apiURL.js
@@ -1,10 +1,11 @@
-import process from 'process';
+require('dotenv').config();
 
 const apiURL = () => {
     let base = '';
-    if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    if (process.env.NODE_ENV === 'development') {
         base = 'http://localhost:8000';
     } else {
+        console.log(process.env.NODE_ENV)
         base = 'https://cinemeld-api.herokuapp.com';
     }
     return base;
